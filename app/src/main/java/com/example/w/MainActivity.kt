e419c0be36d7de30e3db15d4dd526cb2669f5852
sleepingfishboy <2458699092@qq.com>
2023-04-26 09:47:09 +0800

    initial commit

---
+++ app/src/main/java/com/example/w/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.w
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    lateinit var viewModel: MainViewModel
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
